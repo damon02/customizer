652d30212a3d4811a9b5dfc7069d293400d7e509
Damon Asberg <damonasberg@gmail.com>
2020-06-29 21:49:14 +0200

    Added YEEZY SLIDE

---
+++ src/assets/shoes/SLIDE/data.ts
@@ -0,0 +1,33 @@
+import { IGenericProduct } from '../../../@types/types'
+import { DEFAULT_COLORS } from '../../../utils/constants'
+
+import YZYSLIDE from './YZYSLIDE.png'
+
+export const ShoeYZYSlides: IGenericProduct = {
+  id: 'yeezySlides',
+  brand: 'adidas',
+  name: 'YEEZY SLIDE',
+  enabled: true,
+  type: 'SHOE',
+  assets: [
+    {
+      id: 'upper',
+      name: 'Slide',
+      category: 'shoe',
+      presets: DEFAULT_COLORS,
+      zindex: 1,
+      variants: [
+        { 
+          id: 'uppper', 
+          file: YZYSLIDE, 
+          name: 'Slide'
+        }
+      ],
+      overrides: {
+        brightness: { min: 0, max: 3 }
+      }
+    }
+  ]
+}
+
+export default ShoeYZYSlides
--- src/utils/products.ts
+++ src/utils/products.ts
@@ -2,10 +2,12 @@ import { IGenericProduct } from '../@types/types'
 
 import YEEZY350V2 from '../assets/shoes/350V2/data'
 import YEEZY700V1 from '../assets/shoes/700V1/data'
+import YEEZYSLIDE from '../assets/shoes/SLIDE/data'
 
 export const SHOES_YEEZY: IGenericProduct[] = [
   YEEZY350V2,
-  YEEZY700V1
+  YEEZY700V1,
+  YEEZYSLIDE
 ]
 
 
