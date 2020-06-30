62371632b13b0daed2d3c5d7916351cd0fff958e
Damon Asberg <damonasberg@gmail.com>
2020-07-01 00:17:47 +0200

    Added AJ1 version 1

---
+++ src/assets/shoes/AJ1/data.ts
@@ -0,0 +1,283 @@
+import { IGenericProduct } from '../../../@types/types'
+import { DEFAULT_COLORS } from '../../../utils/constants'
+
+import AIRJordanLogo from './airjordan13.png'
+import Heel from './heel9.png'
+import Laces from './laces14.png'
+import Lining from './lining2.png'
+import Outsole from './outsole11.png'
+import Sole from './sole10.png'
+import Swoosh from './swoosh12.png'
+import Toebox from './toebox3.png'
+import ToeboxSides from './toebox_sides8.png'
+import Tongue from './tongue1.png'
+import Upper from './upper5.png'
+import UpperFront from './upper_front7.png'
+import UpperHigh from './upper_high4.png'
+import UpperLacesPanel from './upper_laces_panel6.png'
+
+export const Shoe350V2: IGenericProduct = {
+  id: 'AJ1',
+  type: 'SHOE',
+  brand: 'Nike',
+  name: 'Air Jordan 1 Retro High',
+  enabled: true,
+  description: 'The classic',
+  assets: [
+    {
+      id: 'tongue',
+      name: 'Tongue',
+      zindex: 1,
+      toggleable: false,
+      category: 'tongue',
+      overrides: {
+        brightness: { max: 6 }
+      },
+      presets: DEFAULT_COLORS,
+      variants: [
+        {
+          id: 'tongue',
+          file: Tongue,
+          name: 'Default',
+        },
+      ],
+      default: { brightness: 6, saturation: 0, hue: 0, sepia: 0, display: 'block' }
+    },
+    {
+      id: 'lining',
+      name: 'Lining',
+      zindex: 2,
+      toggleable: false,
+      category: 'upper',
+      overrides: {
+        brightness: { max: 3 }
+      },
+      presets: DEFAULT_COLORS,
+      variants: [
+        {
+          id: 'lining',
+          file: Lining,
+          name: 'Default',
+        },
+      ],
+    },
+    {
+      id: 'toebox',
+      name: 'Toebox',
+      zindex: 3,
+      toggleable: false,
+      category: 'upper',
+      overrides: {
+        brightness: { max: 3 }
+      },
+      presets: DEFAULT_COLORS,
+      variants: [
+        {
+          id: 'toebox',
+          file: Toebox,
+          name: 'Default',
+        },
+      ],
+    },
+    {
+      id: 'upperHigh',
+      name: 'Upper High',
+      zindex: 4,
+      toggleable: false,
+      category: 'upper',
+      overrides: {
+        brightness: { max: 3 }
+      },
+      presets: DEFAULT_COLORS,
+      variants: [
+        {
+          id: 'upperHigh',
+          file: UpperHigh,
+          name: 'Default',
+        },
+      ],
+    },
+    {
+      id: 'upper',
+      name: 'Upper',
+      zindex: 5,
+      toggleable: false,
+      category: 'upper',
+      overrides: {
+        brightness: { max: 3 }
+      },
+      presets: DEFAULT_COLORS,
+      variants: [
+        {
+          id: 'upper',
+          file: Upper,
+          name: 'Default',
+        },
+      ],
+    },
+    {
+      id: 'upperLacesPanel',
+      name: 'Upper Laces Panel',
+      zindex: 6,
+      toggleable: false,
+      category: 'upper',
+      overrides: {
+        brightness: { max: 3 }
+      },
+      presets: DEFAULT_COLORS,
+      variants: [
+        {
+          id: 'upperLacesPanel',
+          file: UpperLacesPanel,
+          name: 'Default',
+        },
+      ],
+    },
+    {
+      id: 'upperFront',
+      name: 'Upper Front',
+      zindex: 7,
+      toggleable: false,
+      category: 'upper',
+      overrides: {
+        brightness: { max: 3 }
+      },
+      presets: DEFAULT_COLORS,
+      variants: [
+        {
+          id: 'upperFront',
+          file: UpperFront,
+          name: 'Default',
+        },
+      ],
+    },
+    {
+      id: 'toeboxSides',
+      name: 'Toebox sides',
+      zindex: 8,
+      toggleable: false,
+      category: 'upper',
+      overrides: {
+        brightness: { max: 3 }
+      },
+      presets: DEFAULT_COLORS,
+      variants: [
+        {
+          id: 'toeboxSides',
+          file: ToeboxSides,
+          name: 'Default',
+        },
+      ],
+    },
+    {
+      id: 'heel',
+      name: 'Heel',
+      zindex: 9,
+      toggleable: false,
+      category: 'upper',
+      overrides: {
+        brightness: { max: 3 }
+      },
+      presets: DEFAULT_COLORS,
+      variants: [
+        {
+          id: 'heel',
+          file: Heel,
+          name: 'Default',
+        },
+      ],
+    },
+    {
+      id: 'sole',
+      name: 'Midsole',
+      zindex: 10,
+      toggleable: false,
+      category: 'sole',
+      overrides: {
+        brightness: { max: 3 }
+      },
+      presets: DEFAULT_COLORS,
+      variants: [
+        {
+          id: 'midsole',
+          file: Sole,
+          name: 'Default',
+        },
+      ],
+    },
+    {
+      id: 'outsole',
+      name: 'Outsole',
+      zindex: 11,
+      toggleable: true,
+      category: 'sole',
+      overrides: {
+        brightness: { max: 3 }
+      },
+      presets: DEFAULT_COLORS,
+      variants: [
+        {
+          id: 'outsole',
+          file: Outsole,
+          name: 'Default',
+        },
+      ],
+    },
+    {
+      id: 'swoosh',
+      name: 'Swoosh',
+      zindex: 12,
+      toggleable: true,
+      category: 'sole',
+      overrides: {
+        brightness: { max: 3 }
+      },
+      presets: DEFAULT_COLORS,
+      variants: [
+        {
+          id: 'swooshNormal',
+          file: Swoosh,
+          name: 'Normal Nike Swoosh',
+        },
+      ],
+    },
+    {
+      id: 'aj1logo',
+      name: 'Logo Upper High',
+      zindex: 13,
+      toggleable: true,
+      category: 'sole',
+      overrides: {
+        brightness: { max: 3 }
+      },
+      presets: DEFAULT_COLORS,
+      variants: [
+        {
+          id: 'ajangelslogo',
+          file: AIRJordanLogo,
+          name: 'Air Jordan Angels logo',
+        },
+      ],
+    },
+    {
+      id: 'laces',
+      name: 'Laces',
+      zindex: 14,
+      toggleable: false,
+      category: 'laces',
+      overrides: {
+        brightness: { max: 3 }
+      },
+      presets: DEFAULT_COLORS,
+      variants: [
+        {
+          id: 'laces',
+          file: Laces,
+          name: 'Default',
+        },
+      ],
+    },
+  ],
+}
+
+export default Shoe350V2
--- src/utils/products.ts
+++ src/utils/products.ts
@@ -2,13 +2,16 @@ import { IGenericProduct } from '../@types/types'
 
 import YEEZY350V2 from '../assets/shoes/350V2/data'
 import YEEZY700V1 from '../assets/shoes/700V1/data'
+import AJ1 from '../assets/shoes/AJ1/data'
 import YEEZYSLIDE from '../assets/shoes/SLIDE/data'
+import { sortByKey } from './general'
 
-export const SHOES_YEEZY: IGenericProduct[] = [
+export const SHOES_YEEZY: IGenericProduct[] = sortByKey([
   YEEZY350V2,
   YEEZY700V1,
-  YEEZYSLIDE
-]
+  YEEZYSLIDE,
+  AJ1
+], 'name')
 
 
 export const ALL_PRODUCTS = [...SHOES_YEEZY]
